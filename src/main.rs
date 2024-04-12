906a8741a59665d64ce98a3e0aea6184c953f1c0
Phylliida <phylliida.dev@gmail.com>
2024-04-12 16:54:55 -0400

    added dep

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 // from https://github.com/Charles-Schleich/WebRTC-in-Rust/blob/master/signalling-server/src/signalling-server.rs
+
+// from https://github.com/Charles-Schleich/WebRTC-in-Rust/blob/master/
+use serde::{Deserialize, Serialize};
+
+
 use std::fs::File;
 use std::{
     collections::HashMap,
@@ -27,6 +32,63 @@ type SessionList = Arc<Mutex<HashMap<SessionID, SessionMembers>>>;
 
 const LOG_FILE: &str = "signalling_server_prototype.log";
 
+pub const SERVER_PORT: &str = "9000";
+
+// The reason im wrapping the IDs in SessionID and UserID is so that rust can type check for us that we arent accidentally using the wrong ID type in the wrong place.
+
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, Hash)]
+pub struct SessionID(String);
+
+impl SessionID {
+    pub fn new(inner: String) -> Self {
+        SessionID(inner)
+    }
+    pub fn inner(self) -> String {
+        self.0
+    }
+}
+
+impl From<&str> for SessionID {
+    fn from(session_id: &str) -> Self {
+        SessionID(session_id.to_string())
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, Hash)]
+pub struct UserID(String);
+
+impl UserID {
+    pub fn new(inner: String) -> Self {
+        UserID(inner)
+    }
+    pub fn inner(self) -> String {
+        self.0
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum SignalEnum {
+    // Return called by the server as soon as the user connects
+    NewUser(UserID),
+
+    // To manage a live session between two users
+    SessionNew,
+    SessionReady(SessionID),
+    SessionJoin(SessionID),
+    SessionJoinSuccess(SessionID),
+    SessionJoinError(SessionID),
+
+    // When Connecting to a Session
+    VideoOffer(String, SessionID),
+    VideoAnswer(String, SessionID),
+    IceCandidate(String, SessionID),
+    ICEError(String, SessionID),
+
+    //
+    Debug,
+}
+
+
 #[derive(Debug, Clone)]
 struct SessionMembers {
     host: UserID,
@@ -51,7 +113,6 @@ fn setup_logging() -> Result<(), SetLoggerError> {
 }
 
 // Get Server IP
-use shared_protocol::{SessionID, SignalEnum, UserID};
 use std::net::UdpSocket;
 
 pub fn get_local_ip() -> Option<String> {
